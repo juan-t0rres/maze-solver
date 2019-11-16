ebe26e1eb07a498674ba075b024fbff8894b6e1e
juan-t0rres <34136394+juan-t0rres@users.noreply.github.com>
2019-11-16 17:46:44 -0500

    Add files via upload

---
+++ cell.js
@@ -0,0 +1,27 @@
+function cell(r,c,size){
+  this.r = r;
+  this.c = c;
+  this.x = c*(size);
+  this.y = r*(size);
+  this.f = Number.MAX_SAFE_INTEGER;
+  this.g = Number.MAX_SAFE_INTEGER;
+  this.h = Number.MAX_SAFE_INTEGER;
+  this.parent = null;
+  this.walkable = true;
+  this.col = color(255);
+
+  this.clicked = function() {
+    var px = mouseX - this.x;
+    var py = mouseY - this.y;
+    if(px >= 0 && px < size && py >= 0 && py < size){
+      this.col = color(0);
+      this.walkable = false;
+    }
+  }
+
+  this.display = function() {
+      stroke(0);
+      fill(this.col);
+      rect(this.x,this.y,size,size);
+  }
+}
